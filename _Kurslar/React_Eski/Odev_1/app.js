2fda7753c4a3548e02cde38f243d3bae51c59524
Fatih SOYSAL : Sessiz Çığlık'larla Dâhi - Ezber YAZAN - YALNIZ Bir RÜZGAR <106076235+fsoysall@users.noreply.github.com>
2022-09-15 23:22:03 +0300

    Delete _Kurslar/React_Eski directory

--- _Kurslar/React_Eski/Odev_1/app.js
+++
@@ -1,5 +0,0 @@
-import getData from "./getData.js";
-import { log, log2 } from '../my-module.js';
-
-
-getData(1).then(d => log(d));
--- _Kurslar/React_Eski/hello-react/src/ComponentS/header.js
+++
@@ -1,9 +0,0 @@
-
-export function Ba2() { return <div>SelaMMM:h:0</div>; }
-
-export function Header() { return <div>SelaMMM:h:1</div>; }
-export function HHeader() { return <div>SelaMMM:h:22</div>; }
-export function HHeader2() { return <div>SelaMMM:h:333</div>; }
-
-
-
